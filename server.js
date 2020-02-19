e31c28fe7bacb0445a323df7348fcfb6e3e6bf70
mitaksim1 <miriamt.simonnet@gmail.com>
2020-02-19 12:45:10 +0100

    Add and config nunjucks

--- server.js
+++ server.js
@@ -1,17 +1,34 @@
+////// CONFIGURATION SERVEUR //////////////
+
 // Import express
 const express = require("express");
-
 // server va recevoir les fonctionnalités d'express
 const server = express();
 
-// Chemin qu'on veut accéder
+////////////////////////////////////////////
+
+
+/////// CONFIGURATION TEMPLATE ENGINE ////////////
+const nunjucks = require("nunjucks");
+nunjucks.configure("./", {
+    express: server
+})
+
+
+////////// CONFIGURATION DE LA PRESENTATION DE LA PAGE ///////////
+
 // on demande à accéder à /, une fois que c'est fait
 // la fonction s'éxecute avec les deux paramètres req et res
 server.get("/", function(req, res) {
     return res.send("ok, je suis arrivée ici");
-})
+});
 
-// on écoute le port 3000
+///////////////////////////////////////////////////////////////////
+
+
+
+///////////// APPEL AU SERVEUR ET PERMISSION POUR ACCEDER AU PORT 3000 /////////
+
 server.listen(3000, function() {
     console.log("Serveur ok")
 });
